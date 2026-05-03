2694fdfd7e5a8234ba392cea51110115881cd409
Piyush Kumar Singh <singhpiyush2002@gmail.com>
2026-05-03 20:12:58 +0530

    Inter-Service Synchronous Communication in different ways

---
+++ additional/interservice/consumer/src/main/java/com/example/consumer/restclient/RestClientController.java
@@ -0,0 +1,21 @@
+package com.example.consumer.restclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/rest-client")
+public class RestClientController {
+
+    private final ProviderRestClient providerRestClient;
+
+    @GetMapping("/instance")
+    public String getInstance() {
+        return providerRestClient.getInstanceInfo();
+    }
+
+}
---
+++ additional/interservice/consumer/src/main/java/com/example/consumer/webclient/WebClientController.java
@@ -0,0 +1,23 @@
+package com.example.consumer.webclient;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/web-client")
+public class WebClientController {
+
+    private final ProviderWebClient providerWebClient;
+
+    @GetMapping("/instance")
+    public Mono<String> getInstance() {
+        return providerWebClient.getInstanceInfo();
+    }
+
+}
