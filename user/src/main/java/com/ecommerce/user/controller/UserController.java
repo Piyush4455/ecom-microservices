c2218052ac2141e9b4b7237661a2d43cd4d735dd
Piyush Kumar Singh <singhpiyush2002@gmail.com>
2026-06-19 23:34:29 +0530

    code for ther gateway

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ecommerce.user.controller;
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +14,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     @Autowired
     UserService userService;
+
+//    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
@@ -25,6 +31,14 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+        log.info("Request received for user: {}", id);
+
+        log.trace("This is TRACE level - Very Detailed logs");
+        log.debug("This is DEBUG level - Used for development debugging");
+        log.info("This is INFO level - General System Information");
+        log.warn("This is WARN level - Something might be wrong");
+        log.error("This is ERROR level - Something failed");
+
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
