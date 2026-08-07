a7f6f6ef3b537803151cc0970ecd88053e0314c7
Piyush Kumar Singh <singhpiyush2002@gmail.com>
2026-08-07 22:42:05 +0530

    Added resilience4j Circuit Breaker to the Order-Service

--- order/src/main/java/com/ecommerce/order/service/CartService.java
+++ order/src/main/java/com/ecommerce/order/service/CartService.java
@@ -7,6 +7,7 @@ import com.ecommerce.order.dto.ProductResponse;
 import com.ecommerce.order.dto.UserResponse;
 import com.ecommerce.order.model.CartItem;
 import com.ecommerce.order.repository.CartItemRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +26,7 @@ public class CartService {
 
     private final UserServiceClient userServiceClient;
 
+    @CircuitBreaker(name = "productService")
     public boolean addToCart(String userId, CartItemRequest request) {
 //        // Look for product
         ProductResponse productResponse = productServiceClient.getProductDetails(Long.valueOf(request.getProductId()));
