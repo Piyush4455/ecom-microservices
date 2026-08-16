1144571d8024a10e8abcad28bfd44235ad191c78
Piyush Kumar Singh <singhpiyush2002@gmail.com>
2026-08-16 22:56:18 +0530

    Setting up Consumer groups.

--- additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "my-topic", groupId = "my-group", concurrency = "3")
-    public void listen(String message) {
-        System.out.println("Received Message: " + message);
+    @KafkaListener(topics = "my-topic", groupId = "test-group-1")
+    public void listen1(String message) {
+        System.out.println("Received Message 1: " + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "test-group-2")
+    public void listen2(String message) {
+        System.out.println("Received Message 2: " + message);
     }
 
 }
